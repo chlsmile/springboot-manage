8743e8b3c0f5be77000bafd67d45d23e3f20b731
chenhongliang001 <chenhongliang001@ke.com>
2021-03-10 12:26:49 +0800

    add

---
+++ src/main/java/com/chlsmile/SpringbootManageApplication.java
@@ -0,0 +1,13 @@
+package com.chlsmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootManageApplication.class, args);
+    }
+
+}
